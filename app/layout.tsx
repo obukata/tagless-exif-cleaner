8f329ba80ea7bbf2c15728e8c7b27b476948c070
obukata <obukata@mj-star.jp>
2026-02-09 13:06:23 +0900

    favicon 設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,30 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "GPS情報や回転情報をマルっと削除！ Tagless.",
+  title: "Tagless. // GPS情報や回転情報をマルっと削除！",
   description: "写真をそのままWebに投稿すると、知らないうちに位置情報や撮影データが残っていることがあります。Tagless. なら、GPS情報や回転情報をまとめて削除して、安全な画像にしてから共有できます。",
+
+  openGraph: {
+    title: "Tagless. // GPS情報や回転情報をマルっと削除！",
+    description: "写真をそのままWebに投稿すると、知らないうちに位置情報や撮影データが残っていることがあります。Tagless",
+    siteName: "Tagless.（タグレス）",
+    url: "https://tagless-exif-cleaner.vercel.app/",
+    type: "website",
+    // images: [
+    //   {
+    //     url: "https://tagless-exif-cleaner.vercel.app/ogp.png"
+    //   }
+    // ]
+  },
+
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon.svg", sizes: "any", type: "image/svg+xml" },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
